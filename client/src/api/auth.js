a9796d5010280cb9d80fd7f0599300d65ae97963
daw2-ramirez22 <ramirezsanchezcristian@fpllefia.com>
2024-04-25 23:29:22 +0200

    header de cookie

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,6 +2,14 @@
 import axios from "./axios";
 
 //utilizo axios para hacer mis "promesas" para los usuarios
-export const registerRequest = (user) => axios.post(`/register`, user)
+export const registerRequest = (user) => {
+    document.cookies()
+    console.log(document.cookies)
+    return axios.post(`/register`, user, {
+    //   headers: {
+    //     'Cookie': cookies // Asegúrate de tener los cookies en el formato adecuado
+    //   }
+    });
+  }
 export const loginRequest = (user) => axios.post(`/login`, user)
 export const vertyTokenRequet = () => axios.get(`/verify`)
