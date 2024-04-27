6673d2f75035d74f6415ac52f31b61e427aaadaa
daw2-ramirez22 <ramirezsanchezcristian@fpllefia.com>
2024-04-27 21:25:09 +0200

    devuelvo la ruta de peticion entre servidores

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //creo la instancia base de todas las promesas luego la importare en axios en task y auth directamente desde aqui
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://back-c19j.onrender.com/api",
     withCredentials: true
 })
 //exportar instance
